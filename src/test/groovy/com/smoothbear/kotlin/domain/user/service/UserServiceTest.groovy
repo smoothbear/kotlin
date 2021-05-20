6b4fa51a98afe4d1291c160e9fd2c006f9e23c11
smoothbear <kjbin0420@naver.com>
2021-05-21 08:40:13 +0900

    [ADD] 유저 테스트 케이스 다양하게 추가

--- src/test/groovy/com/smoothbear/kotlin/domain/user/service/UserServiceTest.groovy
+++ src/test/groovy/com/smoothbear/kotlin/domain/user/service/UserServiceTest.groovy
@@ -5,9 +5,11 @@ import com.smoothbear.kotlin.domain.user.domain.User
 import com.smoothbear.kotlin.domain.user.exception.UserAlreadyExistsException
 import com.smoothbear.kotlin.domain.user.repository.UserRepository
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class UserServiceTest extends Specification {
-    def "검증된 RegisterReq를 받아 회원가입을 진행하면 예외 없이 성공한다"() {
+    @Unroll
+    def "RegisterReq를 받아 데이터베이스에 삽입하면 예외 없이 성공한다 [이메일: #email, 패스워드: #password, 이름: #name]"() {
         given:
         def userRepository = Mock(UserRepository)
         UserService userService = new UserServiceImpl(userRepository)
@@ -16,8 +18,7 @@ class UserServiceTest extends Specification {
         userService.register(new RegisterReq(email, password, name))
 
         then:
-        userRepository.findById("smoothbear@gmail.com") >> null
-        userRepository.findById(email) >> new User(0, email, password, name)
+        userRepository.findById(email) >> Optional.empty()
 
         notThrown UserAlreadyExistsException
 
@@ -25,6 +26,26 @@ class UserServiceTest extends Specification {
         email                  | password   | name
         "smoothbear@gmail.com" | "11111111" | "smoothbear"
         "tester@gmail.com"     | "11111111" | "tester"
-        "tester@gmail.com"     | "test1111" | "tester"
+        "test@gmail.com"       | "test1111" | "tester"
+    }
+
+    @Unroll
+    def "DB에 이미 있는 이메일로 데이터베이스에 삽입하면 UserAlreadyExistsException이 발생한다 [이메일: #email, 패스워드: #password, 이름: #name]"() {
+        given:
+        def userRepository = Mock(UserRepository)
+        UserService userService = new UserServiceImpl(userRepository)
+
+        when:
+        userService.register(new RegisterReq(email, password, name))
+
+        then:
+        userRepository.findById(email) >> Optional.of(new User(null, email, password, name))
+        thrown UserAlreadyExistsException
+
+        where:
+        email              | password   | name
+        "test@gmail.com"   | "11111111" | "tester"
+        "tester@gmail.com" | "testtest" | "test"
+        "user@gmail.com"   | "test1111" | "user"
     }
 }
